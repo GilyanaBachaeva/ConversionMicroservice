b2774e04fde4421bb787de960351d4054cbb9ecf
Gilyana Bachaeva <gelchik29@icloud.com>
2025-08-03 16:24:46 +0300

    Initial commit

---
+++ src/main/java/com/example/ConversionMicroservice/ConversionMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ConversionMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversionMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversionMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ConversionMicroservice/ConversionMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConversionMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
