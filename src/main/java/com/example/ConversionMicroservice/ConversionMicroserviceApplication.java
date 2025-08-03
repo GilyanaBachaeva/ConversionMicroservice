0bff6f74746804476a7c40c88e6ccf4d0f953035
Gilyana Bachaeva <gelchik29@icloud.com>
2025-08-03 16:29:51 +0300

    Update ConversionMicroserviceApplication.java

--- src/main/java/com/example/ConversionMicroservice/ConversionMicroserviceApplication.java
+++ src/main/java/com/example/ConversionMicroservice/ConversionMicroserviceApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConversionMicroserviceApplication {
-
 	public static void main(String[] args) {
+		System.out.println("This is Conversion Microservice");
 		SpringApplication.run(ConversionMicroserviceApplication.class, args);
 	}
-
 }
